90526e3a2753f61dc6994d8fffc4caa0d4c1b36a
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-09-01 10:37:33 -0500

    code implementation of the methods for the client service

--- src/main/java/com/test/alianza/service/ClientServiceImpl.java
+++ src/main/java/com/test/alianza/service/ClientServiceImpl.java
@@ -1,35 +1,44 @@
 package com.test.alianza.service;
 
 import com.test.alianza.model.dto.ClientDto;
+import com.test.alianza.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ClientServiceImpl implements ClientService{
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
     @Override
     public List<ClientDto> getAll() {
-        return null;
+        return repository.getAllClients();
     }
 
     @Override
     public Optional<ClientDto> findBySharedKey(String sharedKey) {
-        return Optional.empty();
+        return repository.GetClientBySharedKey(sharedKey);
     }
 
     @Override
     public Optional<ClientDto> findByBusinessId(String businessId) {
-        return Optional.empty();
+        return repository.getBookByBusinessId(businessId);
     }
 
     @Override
     public ClientDto save(ClientDto clientDto) {
-        return null;
+        return repository.saveClient(clientDto);
     }
 
     @Override
     public boolean deleteBySharedKey(String sharedKey) {
-        return false;
+        return this.findBySharedKey(sharedKey).map(client -> {
+            repository.deleteClient(sharedKey);
+            return true;
+        }).orElse(false);
     }
 }
